e24cb2a910745ade319fc90986120fa25f94bc33
Hector201-david <82106241+Hector201-david@users.noreply.github.com>
2024-12-17 20:59:49 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,15 +1,3 @@
-document.getElementById('sendSmsButton').addEventListener('click', function() {
-    var phoneNumber = '+5355639119'; // Formato: +1234567890
-    var message = 'Este es un mensaje de prueba enviado automáticamente.';
-
-    var xhr = new XMLHttpRequest();
-    xhr.open('POST', 'send_sms.php', true);
-    xhr.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
-    xhr.onreadystatechange = function() {
-        if (xhr.readyState == 4 && xhr.status == 200) {
-            alert('SMS enviado: ' + xhr.responseText);
-            window.location.href = 'https://wa.me/message/TOY2HF6CMOI7B1';
-        }
-    };
-    xhr.send('to=' + encodeURIComponent(phoneNumber) + '&message=' + encodeURIComponent(message));
+document.getElementById('sendButton').addEventListener('click', function() {
+    window.location.href = 'https://wa.me/message/TOY2HF6CMOI7B1';
 });
